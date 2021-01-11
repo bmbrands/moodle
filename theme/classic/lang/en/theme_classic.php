c8a7c23beefa5525202a32bc99ad508a270a4611
Bas Brands <bas@moodle.com>
2021-01-11 16:46:12 +0100

    MDL-70208: frontend moremenu

--- theme/classic/lang/en/theme_classic.php
+++ theme/classic/lang/en/theme_classic.php
@@ -28,8 +28,6 @@ defined('MOODLE_INTERNAL') || die();
 // Raw SCSS setting.
 $string['choosereadme'] = 'Classic is a highly-customisable theme, based on Boost, with an alternative navigation layout. This theme is intended to be used directly, or as a parent theme when creating or updating custom themes to utilise Bootstrap 4.';
 $string['configtitle'] = 'Classic';
-$string['navbardark'] = 'Use a dark style navbar';
-$string['navbardarkdesc'] = 'Swaps text and background colours for the navbar at the top of the page between dark and light.';
 $string['pluginname'] = 'Classic';
 $string['presetfiles'] = 'Additional theme preset files';
 $string['presetfiles_desc'] = 'Preset files can be used to dramatically alter the appearance of the theme. See <a href="https://docs.moodle.org/dev/Boost_Presets">Boost presets</a> for information on creating and sharing your own preset files, and see the <a href="https://archive.moodle.net/boost">Presets repository</a> for presets that others have shared.';
@@ -39,3 +37,7 @@ $string['region-side-post'] = 'Right';
 $string['region-side-pre'] = 'Left';
 $string['sitetitle'] = 'Site title';
 $string['privacy:metadata'] = 'The Classic theme does not store any personal data.';
+
+// Deprecated since Moodle 4.0
+$string['navbardark'] = 'Use a dark style navbar';
+$string['navbardarkdesc'] = 'Swaps text and background colours for the navbar at the top of the page between dark and light.';
